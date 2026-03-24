0d607e19b8ac1b0a84ae7360ff8743b9e02015b4
aditya-7562 <aditya.12114424@lpu.in>
2026-03-24 16:50:02 +0530

    fix: added a missing endpoint in startup-service

--- Startup-Service/src/main/java/com/founderlink/startup/controller/StartupController.java
+++ Startup-Service/src/main/java/com/founderlink/startup/controller/StartupController.java
@@ -33,190 +33,217 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class StartupController {
 
-    private final StartupService startupService;
+        private final StartupService startupService;
 
-    // ─────────────────────────────────────────
-    // CREATE STARTUP
-    // POST /startup
-    // Called by → FOUNDER
-    // ─────────────────────────────────────────
-    @PostMapping
-    public ResponseEntity<ApiResponse<?>> createStartup(
-            @RequestHeader("X-User-Id") Long founderId,
-            @RequestHeader("X-User-Role") String userRole,
-            @Valid @RequestBody StartupRequestDto requestDto) {
+        // ─────────────────────────────────────────
+        // CREATE STARTUP
+        // POST /startup
+        // Called by → FOUNDER
+        // ─────────────────────────────────────────
+        @PostMapping
+        public ResponseEntity<ApiResponse<?>> createStartup(
+                        @RequestHeader("X-User-Id") Long founderId,
+                        @RequestHeader("X-User-Role") String userRole,
+                        @Valid @RequestBody StartupRequestDto requestDto) {
 
-        log.info("POST /startup - createStartup by founderId: {}, role: {}", founderId, userRole);
+                log.info("POST /startup - createStartup by founderId: {}, role: {}", founderId, userRole);
 
-        if (!userRole.equals("ROLE_FOUNDER")) {
-            log.warn("Access denied for createStartup - role: {}", userRole);
-            throw new ForbiddenAccessException(
-                    "Access denied. Only FOUNDERS " +
-                    "can create startups");
+                if (!userRole.equals("ROLE_FOUNDER")) {
+                        log.warn("Access denied for createStartup - role: {}", userRole);
+                        throw new ForbiddenAccessException(
+                                        "Access denied. Only FOUNDERS " +
+                                                        "can create startups");
+                }
+
+                StartupResponseDto response = startupService
+                                .createStartup(founderId, requestDto);
+
+                return ResponseEntity
+                                .status(HttpStatus.CREATED)
+                                .body(new ApiResponse<>(
+                                                "Startup created successfully",
+                                                response));
         }
 
-        StartupResponseDto response = startupService
-                .createStartup(founderId, requestDto);
+        // ─────────────────────────────────────────
+        // GET ALL STARTUP
+        // GET /startup
+        // ─────────────────────────────────────────
+        @GetMapping
+        public ResponseEntity<ApiResponse<?>> getAllStartups(
+                        @RequestHeader("X-User-Role") String userRole) {
 
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(new ApiResponse<>(
-                        "Startup created successfully",
-                        response));
-    }
+                if (!userRole.equals("ROLE_INVESTOR") &&
+                                !userRole.equals("ROLE_FOUNDER") &&
+                                !userRole.equals("ROLE_COFOUNDER") &&
+                                !userRole.equals("ROLE_ADMIN")) {
+                        throw new ForbiddenAccessException(
+                                        "Access denied");
+                }
 
-    // ─────────────────────────────────────────
-    // GET STARTUP BY ID
-    // GET /startup/{id}
-    // Called by → ALL + FeignClient
-    // No role check needed
-    // ─────────────────────────────────────────
-@GetMapping("/{id}")
-    public ResponseEntity<StartupResponseDto> getStartupById(
-            @PathVariable Long id) {
+                log.info("GET /startup - get all the startups");
+                List<StartupResponseDto> response = startupService
+                                .getAllStartups();
 
-        log.info("GET /startup/{} - getStartupById", id);
-        StartupResponseDto response = startupService
-                .getStartupById(id);
-
-        return ResponseEntity
-                .ok(response);
-    }
-    
-	 // ─────────────────────────────────────────
-	 // GET STARTUP BY ID — FOR USERS
-	 // GET /startup/details/{id}
-	 // External use — returns ApiResponse
-	 // Called by Frontend
-	 // ─────────────────────────────────────────
-	 @GetMapping("/details/{id}")
-	 public ResponseEntity<ApiResponse<?>> getStartupDetails(
-	         @PathVariable Long id) {
-	
-	     log.info("GET /startup/details/{} - getStartupDetails", id);
-	     StartupResponseDto response = startupService
-	             .getStartupById(id);
-	
-	     return ResponseEntity
-	             .ok(new ApiResponse<>(
-	                     "Startup fetched successfully",
-	                     response));
-	 }
-    // ─────────────────────────────────────────
-    // GET STARTUPS BY FOUNDER
-    // GET /startup/founder
-    // Called by → FOUNDER
-    // ─────────────────────────────────────────
-    @GetMapping("/founder")
-    public ResponseEntity<ApiResponse<?>> getStartupsByFounder(
-            @RequestHeader("X-User-Id") Long founderId,
-            @RequestHeader("X-User-Role") String userRole) {
-
-        log.info("GET /startup/founder - founderId: {}, role: {}", founderId, userRole);
-        if (!userRole.equals("ROLE_FOUNDER")) {
-            log.warn("Access denied for getStartupsByFounder - role: {}", userRole);
-            throw new ForbiddenAccessException(
-                    "Access denied. Only FOUNDERS " +
-                    "can view their startups");
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startups fetched successfully",
+                                                response));
         }
 
-        List<StartupResponseDto> response = startupService
-                .getStartupsByFounderId(founderId);
+        // ─────────────────────────────────────────
+        // GET STARTUP BY ID
+        // GET /startup/{id}
+        // Called by → ALL + FeignClient
+        // No role check needed
+        // ─────────────────────────────────────────
+        @GetMapping("/{id}")
+        public ResponseEntity<StartupResponseDto> getStartupById(
+                        @PathVariable Long id) {
 
-        return ResponseEntity
-                .ok(new ApiResponse<>(
-                        "Startups fetched successfully",
-                        response));
-    }
+                log.info("GET /startup/{} - getStartupById", id);
+                StartupResponseDto response = startupService
+                                .getStartupById(id);
 
-    // ─────────────────────────────────────────
-    // UPDATE STARTUP
-    // PUT /startup/{id}
-    // Called by → FOUNDER
-    // ─────────────────────────────────────────
-    @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<?>> updateStartup(
-            @RequestHeader("X-User-Id") Long founderId,
-            @RequestHeader("X-User-Role") String userRole,
-            @PathVariable Long id,
-            @Valid @RequestBody StartupRequestDto requestDto) {
-
-        log.info("PUT /startup/{} - updateStartup by founderId: {}", id, founderId);
-        if (!userRole.equals("ROLE_FOUNDER")) {
-            log.warn("Access denied for updateStartup - role: {}", userRole);
-            throw new ForbiddenAccessException(
-                    "Access denied. Only FOUNDERS " +
-                    "can update startups");
+                return ResponseEntity
+                                .ok(response);
         }
 
-        StartupResponseDto response = startupService
-                .updateStartup(id, founderId, requestDto);
+        // ─────────────────────────────────────────
+        // GET STARTUP BY ID — FOR USERS
+        // GET /startup/details/{id}
+        // External use — returns ApiResponse
+        // Called by Frontend
+        // ─────────────────────────────────────────
+        @GetMapping("/details/{id}")
+        public ResponseEntity<ApiResponse<?>> getStartupDetails(
+                        @PathVariable Long id) {
 
-        return ResponseEntity
-                .ok(new ApiResponse<>(
-                        "Startup updated successfully",
-                        response));
-    }
+                log.info("GET /startup/details/{} - getStartupDetails", id);
+                StartupResponseDto response = startupService
+                                .getStartupById(id);
 
-    // ─────────────────────────────────────────
-    // DELETE STARTUP
-    // DELETE /startup/{id}
-    // Called by → FOUNDER
-    // ─────────────────────────────────────────
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<?>> deleteStartup(
-            @RequestHeader("X-User-Id") Long founderId,
-            @RequestHeader("X-User-Role") String userRole,
-            @PathVariable Long id) {
-
-        log.info("DELETE /startup/{} - deleteStartup by founderId: {}", id, founderId);
-        if (!userRole.equals("ROLE_FOUNDER")) {
-            log.warn("Access denied for deleteStartup - role: {}", userRole);
-            throw new ForbiddenAccessException(
-                    "Access denied. Only FOUNDERS " +
-                    "can delete startups");
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startup fetched successfully",
+                                                response));
         }
 
-        startupService.deleteStartup(id, founderId);
+        // ─────────────────────────────────────────
+        // GET STARTUPS BY FOUNDER
+        // GET /startup/founder
+        // Called by → FOUNDER
+        // ─────────────────────────────────────────
+        @GetMapping("/founder")
+        public ResponseEntity<ApiResponse<?>> getStartupsByFounder(
+                        @RequestHeader("X-User-Id") Long founderId,
+                        @RequestHeader("X-User-Role") String userRole) {
 
-        return ResponseEntity
-                .ok(new ApiResponse<>(
-                        "Startup deleted successfully",
-                        null));
-    }
+                log.info("GET /startup/founder - founderId: {}, role: {}", founderId, userRole);
+                if (!userRole.equals("ROLE_FOUNDER")) {
+                        log.warn("Access denied for getStartupsByFounder - role: {}", userRole);
+                        throw new ForbiddenAccessException(
+                                        "Access denied. Only FOUNDERS " +
+                                                        "can view their startups");
+                }
 
-    // ─────────────────────────────────────────
-    // SEARCH STARTUPS
-    // GET /startup/search
-    // Called by → INVESTOR, FOUNDER, ADMIN
-    // ─────────────────────────────────────────
-    @GetMapping("/search")
-    public ResponseEntity<ApiResponse<?>> searchStartups(
-            @RequestHeader("X-User-Role") String userRole,
-            @RequestParam(required = false) String industry,
-            @RequestParam(required = false) StartupStage stage,
-            @RequestParam(required = false) BigDecimal minFunding,
-            @RequestParam(required = false) BigDecimal maxFunding) {
+                List<StartupResponseDto> response = startupService
+                                .getStartupsByFounderId(founderId);
 
-        log.info("GET /startup/search - industry: {}, stage: {}, role: {}", industry, stage, userRole);
-        if (!userRole.equals("ROLE_INVESTOR") &&
-            !userRole.equals("ROLE_FOUNDER") &&
-            !userRole.equals("ROLE_COFUNDER")&&
-            !userRole.equals("ROLE_ADMIN")) {
-            throw new ForbiddenAccessException(
-                    "Access denied");
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startups fetched successfully",
+                                                response));
         }
 
-        List<StartupResponseDto> response = startupService
-                .searchStartups(
-                        industry,
-                        stage,
-                        minFunding,
-                        maxFunding);
+        // ─────────────────────────────────────────
+        // UPDATE STARTUP
+        // PUT /startup/{id}
+        // Called by → FOUNDER
+        // ─────────────────────────────────────────
+        @PutMapping("/{id}")
+        public ResponseEntity<ApiResponse<?>> updateStartup(
+                        @RequestHeader("X-User-Id") Long founderId,
+                        @RequestHeader("X-User-Role") String userRole,
+                        @PathVariable Long id,
+                        @Valid @RequestBody StartupRequestDto requestDto) {
 
-        return ResponseEntity
-                .ok(new ApiResponse<>(
-                        "Startups fetched successfully",
-                        response));
-    }
+                log.info("PUT /startup/{} - updateStartup by founderId: {}", id, founderId);
+                if (!userRole.equals("ROLE_FOUNDER")) {
+                        log.warn("Access denied for updateStartup - role: {}", userRole);
+                        throw new ForbiddenAccessException(
+                                        "Access denied. Only FOUNDERS " +
+                                                        "can update startups");
+                }
+
+                StartupResponseDto response = startupService
+                                .updateStartup(id, founderId, requestDto);
+
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startup updated successfully",
+                                                response));
+        }
+
+        // ─────────────────────────────────────────
+        // DELETE STARTUP
+        // DELETE /startup/{id}
+        // Called by → FOUNDER
+        // ─────────────────────────────────────────
+        @DeleteMapping("/{id}")
+        public ResponseEntity<ApiResponse<?>> deleteStartup(
+                        @RequestHeader("X-User-Id") Long founderId,
+                        @RequestHeader("X-User-Role") String userRole,
+                        @PathVariable Long id) {
+
+                log.info("DELETE /startup/{} - deleteStartup by founderId: {}", id, founderId);
+                if (!userRole.equals("ROLE_FOUNDER")) {
+                        log.warn("Access denied for deleteStartup - role: {}", userRole);
+                        throw new ForbiddenAccessException(
+                                        "Access denied. Only FOUNDERS " +
+                                                        "can delete startups");
+                }
+
+                startupService.deleteStartup(id, founderId);
+
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startup deleted successfully",
+                                                null));
+        }
+
+        // ─────────────────────────────────────────
+        // SEARCH STARTUPS
+        // GET /startup/search
+        // Called by → INVESTOR, FOUNDER, ADMIN
+        // ─────────────────────────────────────────
+        @GetMapping("/search")
+        public ResponseEntity<ApiResponse<?>> searchStartups(
+                        @RequestHeader("X-User-Role") String userRole,
+                        @RequestParam(required = false) String industry,
+                        @RequestParam(required = false) StartupStage stage,
+                        @RequestParam(required = false) BigDecimal minFunding,
+                        @RequestParam(required = false) BigDecimal maxFunding) {
+
+                log.info("GET /startup/search - industry: {}, stage: {}, role: {}", industry, stage, userRole);
+                if (!userRole.equals("ROLE_INVESTOR") &&
+                                !userRole.equals("ROLE_FOUNDER") &&
+                                !userRole.equals("ROLE_COFUNDER") &&
+                                !userRole.equals("ROLE_ADMIN")) {
+                        throw new ForbiddenAccessException(
+                                        "Access denied");
+                }
+
+                List<StartupResponseDto> response = startupService
+                                .searchStartups(
+                                                industry,
+                                                stage,
+                                                minFunding,
+                                                maxFunding);
+
+                return ResponseEntity
+                                .ok(new ApiResponse<>(
+                                                "Startups fetched successfully",
+                                                response));
+        }
 }
